4dd00ad0bc92639a19fb2febc5bef90355448d01
lidong <1404736731@qq.com>
2020-07-24 17:48:30 +0800

    提交配置

--- src/main/Main.go
+++ src/main/Main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"github.com/liangdas/mqant"
+	"github.com/liangdas/mqant/module"
+)
+
 func main() {
-	
+	app := mqant.CreateApp(
+		module.Debug(true),
+		)
+
+	app.Run(
+		)
 }
